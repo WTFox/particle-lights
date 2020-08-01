274f019f66d8a947088fcb227401f7d8ab12a013
A. Fox <anthonyfox1988@gmail.com>
2020-08-01 12:52:53 -0700

    F - Set to static color, remove serial printlns

--- src/lights.cpp
+++ src/lights.cpp
@@ -2,20 +2,19 @@
 #include "neopixel.h"
 #include "patterns.h"
 
-// #define PIXEL_PIN D5
-// #define PIXEL_COUNT 99
-// #define PIXEL_TYPE WS2811
+#define PIXEL_PIN D5
+#define PIXEL_COUNT 99
+#define PIXEL_TYPE WS2811
 
-// Adafruit_NeoPixel *strip =
-//     new Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+Adafruit_NeoPixel *strip =
+    new Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
 // int brightness = 100;
 // bool onCall = false;
 
 void Lights::setup() {
-    Serial.println("Lights::setup");
-    // strip->begin();
-    // strip->show();
+    strip->begin();
+    strip->show();
     // Particle.variable("brightness", brightness);
     // Particle.variable("onCall", onCall);
     // Particle.function("setBrightness", setBrightness);
@@ -23,13 +22,12 @@ void Lights::setup() {
 }
 
 void Lights::update() {
-    Serial.println("Lights::update");
-    // strip->setBrightness(255);
-    // for (uint16_t i = 0; i < strip->numPixels(); i++) {
-    //     strip->setPixelColor(i, Adafruit_NeoPixel::Color(255, 0, 100));
-    // }
-    // strip->show();
-    // delay(20);
+    strip->setBrightness(255);
+    for (uint16_t i = 0; i < strip->numPixels(); i++) {
+        strip->setPixelColor(i, Adafruit_NeoPixel::Color(255, 0, 100));
+    }
+    strip->show();
+    delay(20);
     // if (onCall) {
     //     strip->setBrightness(200);
     //     colorAll(strip, Adafruit_NeoPixel::Color(255, 0, 100), 20);
--- src/particle-lights.ino
+++ src/particle-lights.ino
@@ -4,15 +4,11 @@
 
 SYSTEM_MODE(AUTOMATIC);
 
-std::vector<Lights *> objects;
+std::vector<IArduinoObject *> objects;
 
 void setup() {
-    Serial.begin(9600);
-
     Lights *lights = new Lights();
     objects.push_back(lights);
-
-    Serial.println(objects.size());
 
     for (uint i = 0; i < objects.size(); i++) {
         objects[i]->setup();
@@ -21,9 +17,6 @@ void setup() {
 
 void loop() {
     for (uint i = 0; i < objects.size(); i++) {
-        Serial.println("main loop");
-        // Serial.println(typeid(objects[i]).name());
         objects[i]->update();
     }
-    delay(5000);
 }
