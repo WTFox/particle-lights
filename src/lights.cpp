74cbbfbb8fe3a03bcc63e8fea64b23792f613d59
A. Fox <anthonyfox1988@gmail.com>
2020-08-01 12:56:57 -0700

    Ft - Reinstate particle functions

--- src/lights.cpp
+++ src/lights.cpp
@@ -9,39 +9,32 @@
 Adafruit_NeoPixel *strip =
     new Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
-// int brightness = 100;
-// bool onCall = false;
+int brightness = 100;
+int setBrightness(String input) {
+    brightness = input.toInt();
+    return 0;
+}
+
+bool onCall = false;
+int toggleOnCall(String input) {
+    onCall = !onCall;
+    return 0;
+}
 
 void Lights::setup() {
     strip->begin();
     strip->show();
-    // Particle.variable("brightness", brightness);
-    // Particle.variable("onCall", onCall);
-    // Particle.function("setBrightness", setBrightness);
-    // Particle.function("toggleOnCall", toggleOnCall);
+    Particle.variable("brightness", brightness);
+    Particle.variable("onCall", onCall);
+    Particle.function("setBrightness", setBrightness);
+    Particle.function("toggleOnCall", toggleOnCall);
 }
 
 void Lights::update() {
-    strip->setBrightness(255);
-    for (uint16_t i = 0; i < strip->numPixels(); i++) {
-        strip->setPixelColor(i, Adafruit_NeoPixel::Color(255, 0, 100));
+    if (onCall) {
+        strip->setBrightness(200);
+        colorAll(strip, Adafruit_NeoPixel::Color(255, 0, 100), 20);
+    } else {
+        rainbow(strip, 20);
     }
-    strip->show();
-    delay(20);
-    // if (onCall) {
-    //     strip->setBrightness(200);
-    //     colorAll(strip, Adafruit_NeoPixel::Color(255, 0, 100), 20);
-    // } else {
-    //     rainbow(strip, 20);
-    // }
 }
-
-// int setBrightness(String input) {
-//     brightness = input.toInt();
-//     return 0;
-// }
-
-// int toggleOnCall(String input) {
-//     onCall = !onCall;
-//     return 0;
-// }
