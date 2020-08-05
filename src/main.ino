83cd8bfd8830f0c12e2072d020300ebcfe93635c
A. Fox <anthonyfox1988@gmail.com>
2020-08-04 20:55:15 -0700

    Move particle functions to bottom of file.

--- src/main.ino
+++ src/main.ino
@@ -7,27 +7,6 @@ SYSTEM_MODE(AUTOMATIC);
 // A reference to a LightController component
 LightController *lightController;
 
-int toggleOnCall(String input) {
-    lightController->setOnCall(!lightController->getOnCall());
-    return 0;
-}
-
-int getOnCall(String input) { return lightController->getOnCall(); }
-
-int setBrightness(String input) {
-    lightController->setBrightness(input.toInt());
-    return 0;
-}
-
-int getBrightness(String input) { return lightController->getBrightness(); }
-
-void registerParticleFunctions() {
-    Particle.function("getBrightness", getBrightness);
-    Particle.function("setBrightness", setBrightness);
-    Particle.function("toggleOnCall", toggleOnCall);
-    Particle.function("getOnCall", getOnCall);
-}
-
 // components hold a collection of IComponent's. These ensure that the methods
 // setup() and update() exist.
 std::vector<IComponent *> components;
@@ -50,3 +29,25 @@ void loop() {
         component->update();
     }
 }
+
+// Particle Functions/Variables
+void registerParticleFunctions() {
+    Particle.function("getBrightness", getBrightness);
+    Particle.function("setBrightness", setBrightness);
+    Particle.function("toggleOnCall", toggleOnCall);
+    Particle.function("getOnCall", getOnCall);
+}
+
+int toggleOnCall(String input) {
+    lightController->setOnCall(!lightController->getOnCall());
+    return 0;
+}
+
+int getOnCall(String input) { return lightController->getOnCall(); }
+
+int setBrightness(String input) {
+    lightController->setBrightness(input.toInt());
+    return 0;
+}
+
+int getBrightness(String input) { return lightController->getBrightness(); }
